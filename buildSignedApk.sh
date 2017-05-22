d35b34dfa0fcb085be750c39b91be85f932c049e
Samson Yang <samsonyang@qnap.com>
2017-05-22 15:04:06 +0800

    加入build signed apk的腳本

--- buildSignedApk.sh
+++ buildSignedApk.sh
@@ -1,9 +1,10 @@
 rm takeitover.apk
 rm takeitover_singed.apk
 cd android && ./gradlew assembleRelease
+cd ../
 mv ./android/app/build/outputs/apk/app-release-unsigned.apk ./
 mv app-release-unsigned.apk takeitover.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore takeitover.keystore -storepass takeitover takeitover.apk takeitover
 zipalign -v 4 takeitover.apk  takeitover_singed.apk
 adb install -r takeitover_singed.apk
-# adb shell am start -n com.banner.takeitover
+adb shell am start -n com.takeitover/.MainActivity
--- index.android.js
+++ index.android.js
@@ -1,53 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import AppWithNavigationState from './src/navigators/AppNavigator';
+import configureStore from './src/store/configureStore';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class TakeItOver extends Component {
+class Root extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Provider store={configureStore()}>
+        <AppWithNavigationState />
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('TakeItOver', () => TakeItOver);
+AppRegistry.registerComponent('TakeItOver', () => Root);
